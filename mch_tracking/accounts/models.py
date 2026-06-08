9c7a06a55663517c5775bcd283f89c7814b7065f
Mac <mac@Macs-MacBook-Air.local>
2026-06-08 10:37:29 +0545

    your message

--- mch_tracking/accounts/models.py
+++ mch_tracking/accounts/models.py
@@ -1,3 +1,11 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+class User(AbstractUser):
+    ROLE_CHOICES = [
+        ('admin', 'Admin'),
+        ('health_worker', 'Health Worker'),
+        ('mother', 'Mother'),
+    ]
+
+    role = models.CharField(max_length=20, choices=ROLE_CHOICES)
--- mch_tracking/mch_tracking/settings.py
+++ mch_tracking/mch_tracking/settings.py
@@ -37,6 +37,16 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'accounts',
+    'mothers',
+    'children',
+    'immunization',
+    'appointments',
+    'notifications',
+    'emergency',
+    'dashboard',
+    'reports',
 ]
 
 MIDDLEWARE = [
@@ -120,3 +130,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+AUTH_USER_MODEL = 'accounts.User'
